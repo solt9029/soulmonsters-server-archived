3e8b5bfbdd0491796fa7906cb05eb304fdcd13c6
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-04-12 15:25:56 +0900

    Delete auth guard from user module

--- src/modules/user.module.ts
+++ src/modules/user.module.ts
@@ -1,9 +1,7 @@
-import { AuthGuard } from './../guards/auth.guard';
 import { UserService } from './../services/user.service';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [AuthGuard],
   providers: [UserService],
 })
 export class UserModule {}
