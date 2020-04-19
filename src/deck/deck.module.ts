7fba42fa3cd288d57a9b29cb8efb9ea62857f958
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-04-19 18:47:32 +0900

    Delete controller

--- src/deck/deck.module.ts
+++ src/deck/deck.module.ts
@@ -1,14 +1,12 @@
 import { DeckResolver } from './deck.resolver';
 import { UserService } from '../user/user.service';
 import { DeckService } from './deck.service';
-import { DeckController } from './deck.controller';
 import { DeckEntity } from './deck.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
 @Module({
   imports: [TypeOrmModule.forFeature([DeckEntity])],
-  controllers: [DeckController],
   providers: [DeckService, UserService, DeckResolver],
 })
 export class DeckModule {}
--- src/deck/save.deck.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, MaxLength } from 'class-validator';
-
-export class SaveDeckDto {
-  @IsNotEmpty()
-  @MaxLength(64)
-  name: string;
-}
